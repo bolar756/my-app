682ca8f9245ca0e30948f294b413989a2b57e44f
bolar756 <bolarinwamartins79@gmail.com>
2024-02-03 16:43:39 +0100

    the best thing that ever happened to me

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ import {Skill} from './components/skills';
 import {Contact} from './components/contact';
 
 function App() {
-  let sections = document.querySelectorAll('section');
-  let navLinks = document.querySelectorAll('nav a');
-  window.onscroll = () => {
-      sections.forEach(sec => {
-          let top = window.scrollY;
-          let offset = sec.offsetTop - 150;
-          let height = sec.offsetHeight;
-          let id = sec.getAttribute('id');
-          if(top >= offset && top < offset + height) {
-              navLinks.forEach(links => {
-                  links.classList.remove('active');
-                  document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-              });
-          };
-      });
-  };
   return (
     <div className="App">
       <Navbar/>
--- src/components/navabar.js
+++ src/components/navabar.js
@@ -1,16 +1,20 @@
 import './navbar.css';
 export const Navbar  = () =>{
     return(
-      <header class="header" >
-      <a href="" class="logo">martins</a>
-      <nav class="navbar">
-          <a href="#home" class="active">Home</a>
-          <a href="#">About</a>
-          <a href="#skills">Skills</a>
-          <a href="#">Portfolio</a>
-          <a href="#contact">Contact</a>
-      </nav>
-  </header>
+        <div class="nav">
+        <a href="" class="logo">Martins</a>
+        <span id="icon">
+          <i class='bx bx-menu'  id ="menu" onclick="close()"></i>
+        </span>
+        <span className='span' >
+          <i class='bx bx-sun' id="sun"></i></span>
+        <ul class="sed" id="navbar" >
+          <li><a href="">Blog</a></li>
+          <li><a href="">signup</a></li>
+          <li><a href="">login</a></li>
+          <li><a href="">projects</a></li>
+        </ul>
+      </div>
     )
 }
 
